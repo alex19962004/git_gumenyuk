928599e798c131c45ee2793e33b43895432959d0
Oleksandr Gumenyuk <alex.gumenyuk1@gmail.com>
2022-09-27 18:44:19 +0300

    add implementation of first task

---
+++ ConsoleApp4(1)/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp4_1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            /*string computerPassword = "administrator";
+            Console.WriteLine("Enter password ...");
+            string password = Console.ReadLine();             
+                 while (password != computerPassword)
+                     {
+                        Console.WriteLine("Enter password ...");
+                        password = Console.ReadLine();
+                      }                     
+            Console.WriteLine("You are authorized as an administrator");*/
+
+            string computerPassword = "administrator";
+            string password;
+            do
+            {
+                Console.WriteLine("Enter password ...");
+                password = Console.ReadLine();
+            }
+            while (password != computerPassword);           
+            Console.WriteLine("You are authorized as an administrator");
+        }
+    }
+}
