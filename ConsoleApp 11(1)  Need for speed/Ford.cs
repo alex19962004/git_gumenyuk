7c4e9c76e899de88ff4eef70ac52bd7093657b2b
Oleksandr Gumenyuk <alex.gumenyuk1@gmail.com>
2022-10-22 20:19:38 +0300

    Started impplementation of Need_for_speed_app

---
+++ ConsoleApp 11(1)  Need for speed/Ford.cs
@@ -0,0 +1,35 @@
+﻿
+namespace ConsoleApp_11_1___Need_for_speed
+{
+    public class Ford : Car
+    {
+        //public static int Distance;
+        //public int RemainDistance;
+        public int CarSpeed;
+        public Ford(int maxCarSpeed, int gameTime, int remainDistance, int acceleration) : base(maxCarSpeed, gameTime, remainDistance, acceleration)
+        {
+            MaxCarSpeed = maxCarSpeed;
+            GameTime = gameTime;
+            RemainDistance = remainDistance;
+            Acceleration = acceleration;
+        }      
+        public override int GetTraveledDistance()
+        {
+
+            for( int i = GameTime; i <= GameTime; i++)
+            {
+                CarSpeed = (i * Acceleration);
+                if (CarSpeed >= MaxCarSpeed)
+                {
+                    CarSpeed = MaxCarSpeed;
+                }
+                RemainDistance -=  CarSpeed * i;
+                if (RemainDistance <= 0)
+                {
+                    return 0;
+                }
+            }
+            return RemainDistance;
+        }
+    }
+}
