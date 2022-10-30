60e3c2d026890753d6f159ed7052beb42eaad85b
Oleksandr Gumenyuk <alex.gumenyuk1@gmail.com>
2022-10-30 14:24:21 +0200

    new

---
+++ TestApplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            Do();
+        }
+
+        static int Do()
+        {
+            return 1;
+        }
+    }
+}
